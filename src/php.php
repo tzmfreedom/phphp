5255b82a72d01f2aa103a977d9dddf3d84037b40
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2019-11-16 11:53:14 +0900

    update

--- src/php.php
+++ src/php.php
@@ -26,15 +26,113 @@ function main(string $code)
     }
 }
 
+class VariableEnvironment
+{
+    /**
+     * @var VariableEnvironment
+     */
+    private $parent;
+
+    /**
+     * @var array
+     */
+    private $store;
+
+    public function __construct(?VariableEnvironment $parent)
+    {
+        $this->parent = $parent;
+        $this->store = [];
+    }
+
+    public function isExists(string $key)
+    {
+        if (array_key_exists($key, $this->store)) {
+            return true;
+        }
+        if (is_null($this->parent)) {
+            return false;
+        }
+        return $this->parent->isExists($key);
+    }
+
+    public function get(string $key)
+    {
+        if (array_key_exists($key, $this->store)) {
+            return $this->store[$key];
+        }
+        if (is_null($this->parent)) {
+            throw new Exception("no exist key"); // TODO: impl
+        }
+        return $this->parent->get($key);
+    }
+
+    public function set(string $key, $value)
+    {
+        if ($this->isExists($key)) {
+            if (array_key_exists($key, $this->store)) {
+                $this->store[$key] = $value;
+                return;
+            }
+            $this->parent->set($key, $value);
+            return;
+        }
+        $this->store[$key] = $value;
+    }
+}
+
+class FunctionObject
+{
+    private $name;
+    private $params;
+    private $stmts;
+
+    public function __construct(string $name, array $params, array $stmts)
+    {
+        $this->name = $name;
+        $this->params = $params;
+        $this->stmts = $stmts;
+    }
+
+    public function call(PhpInterpreter $interpreter)
+    {
+        foreach ($this->stmts as $stmt) {
+            $interpreter->process($stmt);
+        }
+    }
+
+    /**
+     * @param int $index
+     * @return \PhpParser\Node\Param[]
+     */
+    public function getParams(): array
+    {
+        return $this->params;
+    }
+}
+
 class PhpInterpreter
 {
+    /**
+     * @var array
+     */
     private $constMap;
+
+    /**
+     * @var array
+     */
+    private $functions;
+
+    /**
+     * @var VariableEnvironment
+     */
+    private $currentEnv;
 
     function __construct()
     {
         $this->constMap = [
             'PHP_EOL' => new \PhpParser\Node\Scalar\String_(PHP_EOL),
         ];
+        $this->currentEnv = new VariableEnvironment(null);
     }
 
     function process($node)
@@ -47,13 +145,32 @@ class PhpInterpreter
                 echo $returnNode->value;
                 break;
             case Expr\FuncCall::class:
-                switch ($node->name->toString()) {
+                $name = $node->name->toString();
+                switch ($name) {
                     case "var_dump":
                         $returnNode = $this->process($node->args[0]);
                         var_dump($returnNode->value);
                         break;
                 }
+                if ($this->isFunctionExists($node->name->toString())) {
+                    $function = $this->getFunction($name);
+                    $args = [];
+                    foreach ($node->args as $i => $arg) {
+                        $args[$i] = $this->process($arg);
+                    }
+                    foreach ($function->getParams() as $i => $param) {
+                        $this->currentEnv->set($param->var->toString(), $args[i]);
+                    }
+                    $prevEnv = $this->currentEnv;
+                    $this->currentEnv = new VariableEnvironment(null);
+                    $function->call($this, $node->args);
+                    $this->currentEnv = $prevEnv;
+                }
                 return;
+            case \PhpParser\Node\Expr\Assign::class:
+                $expr = $this->process($node->expr);
+                $this->currentEnv->set($node->var->name, $expr);
+                return $expr;
             case \PhpParser\Node\Expr\BinaryOp\Concat::class:
                 $left = $this->process($node->left);
                 $right = $this->process($node->right);
@@ -78,11 +195,34 @@ class PhpInterpreter
                 return $this->process($node->value);
             case \PhpParser\Node\Expr\ConstFetch::class:
                 return $this->constMap[$node->name->toString()];
+            case \PhpParser\Node\Expr\Variable::class:
+                if ($this->currentEnv->isExists($node->name)) {
+                    return $this->currentEnv->get($node->name);
+                }
+                return null;
             case \PhpParser\Node\Scalar\String_::class:
             case \PhpParser\Node\Scalar\DNumber::class:
             case \PhpParser\Node\Scalar\LNumber::class:
                 return $node;
+            case Stmt\Function_::class:
+                $name = $node->name->toString();
+                $this->addFunction($name, $node->params, $node->stmts);
         }
+    }
+
+    public function addFunction(string $name, array $params, array $stmts): void
+    {
+        $this->functions[$name] = new FunctionObject($name, $params, $stmts);
+    }
+
+    public function isFunctionExists(string $name): bool
+    {
+        return array_key_exists($name, $this->functions);
+    }
+
+    public function getFunction(string $name): FunctionObject
+    {
+        return $this->functions[$name];
     }
 }
 $code = file_get_contents("php://stdin");
