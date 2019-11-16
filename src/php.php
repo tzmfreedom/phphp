63bd247e47fb2dc978108643e05ee8ec18a248cd
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2019-11-16 22:59:18 +0900

    update

--- src/php.php
+++ src/php.php
@@ -16,10 +16,10 @@ function main(string $code)
         $interpreter = new PhpInterpreter();
         $ast = $parser->parse($code);
         foreach ($ast as $stmt) {
-            $interpreter->process($stmt);
+            $interpreter->evaluate($stmt);
         }
-    $dumper = new NodeDumper;
-    echo $dumper->dump($ast) . "\n";
+        $dumper = new NodeDumper;
+        echo $dumper->dump($ast) . "\n";
     } catch (Error $error) {
         echo "Parse error: {$error->getMessage()}\n";
         return;
@@ -80,6 +80,47 @@ class VariableEnvironment
     }
 }
 
+class ClassObject
+{
+    private $name;
+    public $methods;
+
+    public function __construct(string $name, array $methods)
+    {
+        $this->name = $name;
+        $this->methods = $methods;
+    }
+}
+
+class MethodObject
+{
+    public $method;
+
+    public function __construct(Stmt\ClassMethod $method)
+    {
+        $this->method = $method;
+    }
+
+    public function call(PhpInterpreter $interpreter)
+    {
+        foreach ($this->method->stmts as $stmt) {
+            $ret = $interpreter->evaluate($stmt);
+            if ($ret instanceof Stmt\Return_) {
+                return $interpreter->evaluate($ret->expr);
+            }
+        }
+        return new NullValue();
+    }
+
+    /**
+     * @return \PhpParser\Node\Param[]
+     */
+    public function getParams(): array
+    {
+        return $this->method->params;
+    }
+}
+
 class FunctionObject
 {
     private $name;
@@ -96,8 +137,12 @@ class FunctionObject
     public function call(PhpInterpreter $interpreter)
     {
         foreach ($this->stmts as $stmt) {
-            $interpreter->process($stmt);
+            $ret = $interpreter->evaluate($stmt);
+            if ($ret instanceof Stmt\Return_) {
+                return $interpreter->evaluate($ret->expr);
+            }
         }
+        return new NullValue();
     }
 
     /**
@@ -110,6 +155,40 @@ class FunctionObject
     }
 }
 
+class NullValue
+{
+    public $value;
+
+    public function __construct()
+    {
+        $this->value = null;
+    }
+}
+
+class Instance
+{
+    public $class;
+    public $properties;
+
+    public function __construct(ClassObject $class)
+    {
+        $this->class = $class;
+        $this->properties = [];
+    }
+}
+
+class BoolObject
+{
+    /**
+     * @var bool
+     */
+    public $value;
+
+    public function __construct(bool $value)
+    {
+        $this->value = $value;
+    }
+}
 class PhpInterpreter
 {
     /**
@@ -127,28 +206,35 @@ class PhpInterpreter
      */
     private $currentEnv;
 
+    /**
+     * @var array
+     */
+    private $classMap;
+
     function __construct()
     {
         $this->constMap = [
             'PHP_EOL' => new \PhpParser\Node\Scalar\String_(PHP_EOL),
+            'true' => new BoolObject(true),
+            'false' => new BoolObject(false),
         ];
         $this->currentEnv = new VariableEnvironment(null);
     }
 
-    function process($node)
+    function evaluate($node)
     {
         switch (get_class($node)) {
             case Stmt\Expression::class:
-                return $this->process($node->expr);
+                return $this->evaluate($node->expr);
             case Echo_::class:
-                $returnNode = $this->process($node->exprs[0]);
+                $returnNode = $this->evaluate($node->exprs[0]);
                 echo $returnNode->value;
                 break;
             case Expr\FuncCall::class:
                 $name = $node->name->toString();
                 switch ($name) {
                     case "var_dump":
-                        $returnNode = $this->process($node->args[0]);
+                        $returnNode = $this->evaluate($node->args[0]);
                         var_dump($returnNode->value);
                         break;
                 }
@@ -156,50 +242,51 @@ class PhpInterpreter
                     $function = $this->getFunction($name);
                     $args = [];
                     foreach ($node->args as $i => $arg) {
-                        $args[$i] = $this->process($arg);
-                    }
-                    foreach ($function->getParams() as $i => $param) {
-                        $this->currentEnv->set($param->var->toString(), $args[i]);
+                        $args[$i] = $this->evaluate($arg);
                     }
                     $prevEnv = $this->currentEnv;
                     $this->currentEnv = new VariableEnvironment(null);
-                    $function->call($this, $node->args);
+                    foreach ($function->getParams() as $i => $param) {
+                        $this->currentEnv->set($param->var->name, $args[$i]);
+                    };
+                    $ret = $function->call($this);
                     $this->currentEnv = $prevEnv;
+                    return $ret;
                 }
-                return;
+                throw new Exception("no function exists");
             case \PhpParser\Node\Expr\Assign::class:
-                $expr = $this->process($node->expr);
+                $expr = $this->evaluate($node->expr);
                 $this->currentEnv->set($node->var->name, $expr);
                 return $expr;
             case \PhpParser\Node\Expr\BinaryOp\Concat::class:
-                $left = $this->process($node->left);
-                $right = $this->process($node->right);
+                $left = $this->evaluate($node->left);
+                $right = $this->evaluate($node->right);
                 return new \PhpParser\Node\Scalar\String_($left->value . $right->value);
             case \PhpParser\Node\Expr\BinaryOp\Plus::class:
-                $left = $this->process($node->left);
-                $right = $this->process($node->right);
+                $left = $this->evaluate($node->left);
+                $right = $this->evaluate($node->right);
                 return new \PhpParser\Node\Scalar\LNumber($left->value + $right->value);
             case \PhpParser\Node\Expr\BinaryOp\Minus::class:
-                $left = $this->process($node->left);
-                $right = $this->process($node->right);
+                $left = $this->evaluate($node->left);
+                $right = $this->evaluate($node->right);
                 return new \PhpParser\Node\Scalar\LNumber($left->value - $right->value);
             case \PhpParser\Node\Expr\BinaryOp\Mul::class:
-                $left = $this->process($node->left);
-                $right = $this->process($node->right);
+                $left = $this->evaluate($node->left);
+                $right = $this->evaluate($node->right);
                 return new \PhpParser\Node\Scalar\LNumber($left->value * $right->value);
             case \PhpParser\Node\Expr\BinaryOp\Div::class:
-                $left = $this->process($node->left);
-                $right = $this->process($node->right);
+                $left = $this->evaluate($node->left);
+                $right = $this->evaluate($node->right);
                 return new \PhpParser\Node\Scalar\LNumber($left->value / $right->value);
             case \PhpParser\Node\Arg::class:
-                return $this->process($node->value);
+                return $this->evaluate($node->value);
             case \PhpParser\Node\Expr\ConstFetch::class:
                 return $this->constMap[$node->name->toString()];
             case \PhpParser\Node\Expr\Variable::class:
                 if ($this->currentEnv->isExists($node->name)) {
                     return $this->currentEnv->get($node->name);
                 }
-                return null;
+                return new NullValue();
             case \PhpParser\Node\Scalar\String_::class:
             case \PhpParser\Node\Scalar\DNumber::class:
             case \PhpParser\Node\Scalar\LNumber::class:
@@ -207,6 +294,39 @@ class PhpInterpreter
             case Stmt\Function_::class:
                 $name = $node->name->toString();
                 $this->addFunction($name, $node->params, $node->stmts);
+                return;
+            case Stmt\Return_::class:
+                return $node;
+            case Expr\New_::class:
+                $name = $node->class->toString();
+                if (array_key_exists($name, $this->classMap)) {
+                    $obj = new Instance($this->classMap[$name]);
+                    return $obj;
+                }
+                throw new Exception("no class exists");
+            case Expr\MethodCall::class:
+                $receiver = $this->currentEnv->get($node->var->name);
+                /** @var $method MethodObject */
+                $method = $receiver->class->methods[$node->name->toString()];
+                $args = [];
+                foreach ($node->args as $i => $arg) {
+                    $args[$i] = $this->evaluate($arg);
+                }
+                if (count($args) < count($method->getParams())) {
+                    throw new Exception('few parameter');
+                }
+                $prevEnv = $this->currentEnv;
+                $this->currentEnv = new VariableEnvironment(null);
+                $this->currentEnv->set('this', $receiver);
+                foreach ($method->getParams() as $i => $param) {
+                    $this->currentEnv->set($param->var->name, $args[$i]);
+                };
+                $ret = $method->call($this);
+                $this->currentEnv = $prevEnv;
+                return $ret;
+            case Stmt\Class_::class:
+                $this->addClass($node);
+                return;
         }
     }
 
@@ -223,6 +343,15 @@ class PhpInterpreter
     public function getFunction(string $name): FunctionObject
     {
         return $this->functions[$name];
+    }
+
+    public function addClass(Stmt\Class_ $node)
+    {
+        $methods = [];
+        foreach ($node->getMethods() as $method) {
+            $methods[$method->name->toString()] = new MethodObject($method);
+        }
+        $this->classMap[$node->name->toString()] = new ClassObject($node->name, $methods);
     }
 }
 $code = file_get_contents("php://stdin");
